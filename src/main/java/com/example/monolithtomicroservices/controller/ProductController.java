c5a522b1ca3369bc3cb6fd9d1158588747e90be5
Jochem De Schepper <jochem.de.schepper@student.howest.be>
2024-05-01 17:21:44 +0200

    Added product controller

---
+++ src/main/java/com/example/monolithtomicroservices/controller/ProductController.java
@@ -0,0 +1,14 @@
+package com.example.monolithtomicroservices.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @GetMapping
+    public void getProducts() {
+        // Get all products
+    }
+}
