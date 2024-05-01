5663cd935d1dc0461f79d8f78dd4ffab946278de
Jochem De Schepper <jochem.de.schepper@student.howest.be>
2024-05-01 16:14:33 +0200

    Initial commit

---
+++ src/main/java/com/example/monolithtomicroservices/MonolithToMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.monolithtomicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolithToMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonolithToMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/monolithtomicroservices/MonolithToMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monolithtomicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithToMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
