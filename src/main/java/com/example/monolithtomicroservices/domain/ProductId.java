a09b5dd921ad8a499dd19076b3d45c922f2d9b57
Jochem De Schepper <jochem.de.schepper@student.howest.be>
2024-05-02 20:42:27 +0200

    Added builder constructor

--- src/main/java/com/example/monolithtomicroservices/domain/ProductId.java
+++ src/main/java/com/example/monolithtomicroservices/domain/ProductId.java
@@ -1,4 +1,29 @@
 package com.example.monolithtomicroservices.domain;
 
-public class ProductId {
+public record ProductId(String id) {
+    private ProductId(Builder builder) {
+        this(builder.id);
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String id;
+
+        public Builder id(long id) {
+            this.id = String.valueOf(id);
+            return this;
+        }
+
+        public Builder id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public ProductId build() {
+            return new ProductId(this.id);
+        }
+    }
 }
