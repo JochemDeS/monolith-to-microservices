8339568e3e93a37c505274e400f517e580eb873b
Jochem De Schepper <jochem.de.schepper@student.howest.be>
2024-05-16 17:45:07 +0200

    Tested register customer use case on failures

--- src/test/java/com/example/monolithtomicroservices/application/customer/RegisterCustomerUseCaseTest.java
+++ src/test/java/com/example/monolithtomicroservices/application/customer/RegisterCustomerUseCaseTest.java
@@ -6,48 +6,33 @@ import com.example.monolithtomicroservices.domain.Name;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RegisterCustomerUseCaseTest {
-    private FakeGetCustomerByEmailPort fakeGetCustomerByEmailPort = new FakeGetCustomerByEmailPort();
-    private FakeSaveCustomerPort fakeSaveCustomerPort = new FakeSaveCustomerPort();
+    private final FakeGetCustomerByEmailPort fakeGetCustomerByEmailPort = new FakeGetCustomerByEmailPort();
+    private final FakeSaveCustomerPort fakeSaveCustomerPort = new FakeSaveCustomerPort();
+
+    private final Customer customer = Customer.builder()
+            .name(Name.builder()
+                    .firstName("John")
+                    .lastName("Doe")
+                    .build())
+            .email("john.doe@test.com")
+            .address(Address.builder()
+                    .street("Main Street")
+                    .houseNumber("1")
+                    .city("Springfield")
+                    .zip("12345")
+                    .country("USA")
+                    .build())
+            .build();
 
     @Test
     void shouldRegisterNewCustomer() {
-//        final var customer = Customer.builder()
-//                .name(Name.builder()
-//                                .firstName("John")
-//                                .lastName("Doe")
-//                                .build())
-//                .email("john.doe@test.com")
-//                .address(Address.builder()
-//                        .street("Main Street")
-//                        .houseNumber("1")
-//                        .city("Springfield")
-//                        .zip("12345")
-//                        .country("USA")
-//                        .build())
-//                .build();
-//        fakeGetCustomerByEmailPort.register(customer);
-
-//        final var request = RegisterCustomer.builder()
-//                .customer(customer.name())
-//                .email(customer.email())
-//                .address(customer.address())
-//                .build();
-
         final var request = RegisterCustomer.builder()
-                .customer(Name.builder()
-                        .firstName("John")
-                        .lastName("Doe")
-                        .build())
-                .email("john.doe@test.com")
-                .address(Address.builder()
-                        .street("Main Street")
-                        .houseNumber("1")
-                        .city("Springfield")
-                        .zip("12345")
-                        .country("USA")
-                        .build())
+                .customer(this.customer.name())
+                .email(this.customer.email())
+                .address(this.customer.address())
                 .build();
 
         final var target = new RegisterCustomerUseCase(fakeGetCustomerByEmailPort, fakeSaveCustomerPort);
@@ -55,6 +40,27 @@ class RegisterCustomerUseCaseTest {
 
         final var result = fakeSaveCustomerPort.getCustomer();
         assertThat(result).isNotNull();
+    }
+
+    @Test
+    void shouldThrowExceptionWhenEmailAlreadyExists() {
+        fakeGetCustomerByEmailPort.register(this.customer);
+        final var request = RegisterCustomer.builder()
+                .customer(this.customer.name())
+                .email(this.customer.email())
+                .address(this.customer.address())
+                .build();
+
+        final var target = new RegisterCustomerUseCase(fakeGetCustomerByEmailPort, fakeSaveCustomerPort);
+        assertThatThrownBy(() -> target.handle(request)).isInstanceOf(IllegalStateException.class);
+        assertThat(fakeSaveCustomerPort.getCustomer()).isNull();
+    }
+
+    @Test
+    void shouldThrowExceptionWhenCustomerIsNull() {
+        final var target = new RegisterCustomerUseCase(fakeGetCustomerByEmailPort, fakeSaveCustomerPort);
+        assertThatThrownBy(() -> target.handle(null)).isInstanceOf(IllegalArgumentException.class);
+        assertThat(fakeSaveCustomerPort.getCustomer()).isNull();
     }
 
 }
