94be6224d31be40cc2c8798253682215b8e31ed6
Jochem De Schepper <jochem.de.schepper@student.howest.be>
2024-05-16 17:19:22 +0200

    Removed redundant import

--- src/main/java/com/example/monolithtomicroservices/application/product/GetAllProductsPort.java
+++ src/main/java/com/example/monolithtomicroservices/application/product/GetAllProductsPort.java
@@ -2,7 +2,6 @@ package com.example.monolithtomicroservices.application.product;
 
 import com.example.monolithtomicroservices.domain.Product;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 public interface GetAllProductsPort {
     Page<Product> all(ProductRequest request);
