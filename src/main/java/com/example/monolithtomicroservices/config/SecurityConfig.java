336b6bc8fda0ede64370867762a2e0fe3006b215
Jochem De Schepper <jochem.de.schepper@student.howest.be>
2024-06-22 16:46:28 +0200

    Authenticate endpoints

--- src/main/java/com/example/monolithtomicroservices/config/SecurityConfig.java
+++ src/main/java/com/example/monolithtomicroservices/config/SecurityConfig.java
@@ -1,24 +1,36 @@
 package com.example.monolithtomicroservices.config;
 
+import com.example.monolithtomicroservices.security.JWTRequestFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final JWTRequestFilter jwtRequestFilter;
+
+    public SecurityConfig(JWTRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+                .addFilterBefore(jwtRequestFilter, AuthorizationFilter.class)
+                .authorizeHttpRequests(authorize ->
+                    authorize.requestMatchers("/actuator/**", "/auth/login", "/auth/register").permitAll()
+                            .anyRequest().authenticated()
+                )
                 .httpBasic(withDefaults());
 
         return http.build();
