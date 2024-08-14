c7b6b7cdd82a570844473302e50aa501e266b20b
Jochem De Schepper <jochem.de.schepper@student.howest.be>
2024-08-14 14:00:54 +0200

    Updated flyway migrations

--- src/main/resources/db/migration/V8__create_customers_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS customers(
-    id BIGSERIAL PRIMARY KEY NOT NULL,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    address_id BIGSERIAL,
-    email VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT current_timestamp,
-    CONSTRAINT fk_address_id FOREIGN KEY (address_id) REFERENCES addresses(id)
-);
