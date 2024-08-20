f9cd8f6266f073ed8b2c2920e35915883064dd92
Jochem De Schepper <jochem.de.schepper@student.howest.be>
2024-08-20 19:57:09 +0200

    The quantity can be 0

--- src/main/java/com/example/monolithtomicroservices/infrastructure/http/cart/UpdateCartModel.java
+++ src/main/java/com/example/monolithtomicroservices/infrastructure/http/cart/UpdateCartModel.java
@@ -1,12 +1,13 @@
 package com.example.monolithtomicroservices.infrastructure.http.cart;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record UpdateCartModel(
         @NotNull @Positive
         long productId,
-        @NotNull @Positive
+        @NotNull @Min(0)
         int quantity
 ) {
 }
